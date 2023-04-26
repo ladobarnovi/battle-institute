e0d1dbbbaeccda8622d45ec1ac936a285f990ea6
gg <gg@ofs.com>
2023-04-26 16:28:06 +0400

    added section dividers and other small details

--- pages/course/[course-id].vue
+++ pages/course/[course-id].vue
@@ -14,34 +14,35 @@
             />
         </div>
 
-          <footer class="my-4 text-white space-y-4">
+          <footer class="my-4 text-white space-y-4 p-6">
               <div>
                 <h1 class="text-4xl font-bold text-main-content">Lorem ipsum dolor sit amet</h1>
                 <h2 class="text-lg text-main-content">In metus vulputate eu scelerisque. Id nibh tortor id aliquet lectus proin nibh nisl. Vitae auctor eu augue ut lectus.</h2>
-                <h2 class="text-lg text-main-content">Created by <a href="/instructor/craigjones">Craig Jones</a></h2>
-              </div><div>
-              <p class="text-2xl font-bold text-main-content">About this course</p>
-                <p class="list-disc text-main-content">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Diam volutpat commodo sed egestas. Vel pharetra vel turpis nunc eget lorem dolor sed. Faucibus vitae aliquet nec ullamcorper sit.
-                Gravida quis blandit turpis cursus in hac habitasse. Velit egestas dui id ornare arcu odio. Adipiscing enim eu turpis egestas pretium aenean pharetra.
-                Pulvinar pellentesque habitant morbi tristique senectus. Consectetur purus ut faucibus pulvinar. Lectus proin nibh nisl condimentum.
-                Rhoncus mattis rhoncus urna neque viverra justo nec ultrices. Cursus vitae congue mauris rhoncus aenean vel elit scelerisque. Etiam dignissim diam quis enim lobortis.
-                Urna nunc id cursus metus aliquam eleifend mi. Purus viverra accumsan in nisl. Id ornare arcu odio ut sem nulla. Lorem mollis aliquam ut porttitor.
-                Vestibulum lorem sed risus ultricies tristique nulla aliquet enim tortor. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit.
+                <h2 class="text-lg text-main-content">Created by <a class="font-bold underline" href="/instructor/craigjones">Craig Jones</a></h2>
+              </div>
+              <div class="border-t border-main-content/20 pt-2">
+                <p class="text-2xl font-bold text-main-content">About this course</p>
+                <p class="list-disc text-main-content mt-1">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Diam volutpat commodo sed egestas. Vel pharetra vel turpis nunc eget lorem dolor sed. Faucibus vitae aliquet nec ullamcorper sit.
+                  Gravida quis blandit turpis cursus in hac habitasse. Velit egestas dui id ornare arcu odio. Adipiscing enim eu turpis egestas pretium aenean pharetra.
+                  Pulvinar pellentesque habitant morbi tristique senectus. Consectetur purus ut faucibus pulvinar. Lectus proin nibh nisl condimentum.
+                  Rhoncus mattis rhoncus urna neque viverra justo nec ultrices. Cursus vitae congue mauris rhoncus aenean vel elit scelerisque. Etiam dignissim diam quis enim lobortis.
+                  Urna nunc id cursus metus aliquam eleifend mi. Purus viverra accumsan in nisl. Id ornare arcu odio ut sem nulla. Lorem mollis aliquam ut porttitor.
+                  Vestibulum lorem sed risus ultricies tristique nulla aliquet enim tortor. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit.
                 </p>
-                </div>
-              </footer>
+               </div>
+         </footer>
       </div>
 
-      <div class="player-nav basis-1/4">
+      <div class="player-nav basis-1/4 border-l border-main-content/10">
         <ul class="player-timestamps text-white flex flex-col gap-1">
           <li
             v-for="timestamp in arrTimestamps"
             class="timestamp"
           >
             <button
-              class="px-8 py-4 w-full bg-base-color text-left text-main-content"
+              class="px-8 py-4 w-full text-left text-main-content"
               @click="moveToTime(timestamp.timeSeconds)"
             >
               {{ timestamp.timeFormatted }} - {{ timestamp.label }}
