a62bf5a847484c57be4fb7096200b11d1525225c
gg <gg@ofs.com>
2023-05-02 12:45:06 +0400

    category fix

--- components/MainNavigation/MainNavigation.vue
+++ components/MainNavigation/MainNavigation.vue
@@ -2,7 +2,7 @@
   <div class="main-navigation w-full">
     <div class="wrapper w-full">
       <div class="flex justify-center gap-16 text-white bg-white/10 py-2">
-        <a href="">All Videoshttps://www.youtube.com/watch?v=583T2MxHOA8</a>
+        <a href="">All Videos</a>
         <a href="">Categories</a>
         <a href="">Instructors</a>
         <a href="">Promotions</a>
