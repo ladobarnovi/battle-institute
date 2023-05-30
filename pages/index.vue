fa2b691beaf7265e2fb4a7582c5ba60119456930
gg <gg@ofs.com>
2023-05-30 10:43:16 +0400

    Updated featured courses

--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,62 @@
 <template>
-  <div id="home" class="w-[95vw] mx-auto">
+  <div id="home">
     <div class="py-8">
-      <div class="slider-container">
-        <div class="rounded-xl overflow-hidden">
-          <Swiper
-              :loop="true"
-          >
-            <SwiperSlide>
-              <div class="slide-wrapper relative">
-                <img src="/images/slide-img-1.jpg" alt="">
-                <p class="text-3xl text-main-content mt-4">CQC in Action</p>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div class="slide-wrapper relative">
-                <img src="/images/slide-img-2.jpg" alt="">
-                <p class="text-3xl text-main-content mt-4">TCCC</p>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div class="slide-wrapper relative">
-                <img src="/images/slide-img-3.jpg" alt="">
-                <p class="text-3xl text-main-content mt-4">Basic Techniques and Drills</p>
-              </div>
-            </SwiperSlide>
-          </Swiper>
-        </div>
+      <section class="h-[70vh] hover:bg-base-color-comp py-8">
+      <div class="w-[85vw] mx-auto h-full flex flex-col">
+          <h2 class="text-main-content text-4xl mb-4">Featured Courses</h2>
+          <div class="grid grid-rows-2 grid-flow-col gap-4 grow">
+            <div class="row-span-2 col-span-2 thumbnail-1 rounded-2xl"></div>
+            <div class="col-span-1 bg-white thumbnail-2 rounded-2xl"></div>
+            <div class="col-span-1 bg-black thumbnail-1 rounded-2xl"></div>
+          </div>
       </div>
+  </section>
+      <!--<div class="slider-container">-->
+        <!--<div class="rounded-xl overflow-hidden">-->
+        <!--  <Swiper-->
+        <!--      :loop="true"-->
+        <!--  >-->
+        <!--    <SwiperSlide>-->
+        <!--      <div class="slide-wrapper relative">-->
+        <!--        <img src="/images/slide-img-1.jpg" alt="">-->
+        <!--        <p class="text-3xl text-main-content mt-4">CQC in Action</p>-->
+        <!--      </div>-->
+        <!--    </SwiperSlide>-->
+        <!--    <SwiperSlide>-->
+        <!--      <div class="slide-wrapper relative">-->
+        <!--        <img src="/images/slide-img-2.jpg" alt="">-->
+        <!--        <p class="text-3xl text-main-content mt-4">TCCC</p>-->
+        <!--      </div>-->
+        <!--    </SwiperSlide>-->
+        <!--    <SwiperSlide>-->
+        <!--      <div class="slide-wrapper relative">-->
+        <!--        <img src="/images/slide-img-3.jpg" alt="">-->
+        <!--        <p class="text-3xl text-main-content mt-4">Basic Techniques and Drills</p>-->
+        <!--      </div>-->
+        <!--    </SwiperSlide>-->
+        <!--  </Swiper>-->
+        <!--</div>-->
+      <!--</div>-->
 
     </div>
   </div>
-  <div class="bg-base-color-comp w-full">
-        <CardList
-            class="pt-8 w-[95vw] mx-auto"
-            :arr-cards-data="arrCourses"
-            title="Featured Courses"
-        />
-  </div>
-   <div class="w-[95vw] mx-auto">
-        <CardList
-            class="pt-8 w-[95vw] mx-auto"
-            :arr-cards-data="arrInstructors"
-            title="Featured Courses"
-        />
-  </div>
+
+
+
+  <!--<div class="bg-base-color-comp w-full">-->
+  <!--      <CardList-->
+  <!--          class="pt-8 w-[95vw] mx-auto"-->
+  <!--          :arr-cards-data="arrCourses"-->
+  <!--          title="Featured Courses"-->
+  <!--      />-->
+  <!--</div>-->
+  <!-- <div class="w-[95vw] mx-auto">-->
+  <!--      <CardList-->
+  <!--          class="pt-8 w-[95vw] mx-auto"-->
+  <!--          :arr-cards-data="arrInstructors"-->
+  <!--          title="Featured Instructors"-->
+  <!--      />-->
+  <!--</div>-->
 </template>
 
 <script lang="ts">
@@ -132,4 +145,18 @@ export default {
     }
   }
 }
+
+.thumbnail-1{
+ background-image: url("/images/tn1.png");
+ background-size: cover;
+ background-position: bottom;
+
+}
+
+.thumbnail-2{
+ background-image: url("/images/tn2.png");
+ background-size: cover;
+ background-position: bottom;
+}
+
 </style>
