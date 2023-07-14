c2be77539b7dd43ee176a1b2ec6bfb92f8efcffe
Lado Barnovi <ladobarnovi93@gmail.com>
2023-07-14 20:53:51 +0400

    Instructor page improvements

--- pages/instructor/[instructor-id].vue
+++ pages/instructor/[instructor-id].vue
@@ -3,27 +3,31 @@
     <div class="instructor-info pb-8">
       <div class="left">
         <div class="image-container mb-4">
-
+          <img src="/images/marine.jpg" alt="Profile">
         </div>
 
         <ul class="links text-xl text-white">
           <li>
             <a href="/" target="_blank">
+              <Icon name="uil:globe" color="white" />
               Website
             </a>
           </li>
           <li>
             <a href="/" target="_blank">
+              <Icon name="uil:twitter" color="white" />
               Twitter
             </a>
           </li>
           <li>
             <a href="/" target="_blank">
+              <Icon name="uil:facebook" color="white" />
               Facebook
             </a>
           </li>
           <li>
             <a href="/" target="_blank">
+              <Icon name="uil:instagram" color="white" />
               Instagram
             </a>
           </li>
@@ -41,7 +45,7 @@
         </ul>
 
         <p class="text-3xl font-bold mt-8 mb-2">About Me</p>
-        <div class="about">
+        <div class="about text-md">
           <p>Mi quis hendrerit dolor magna eget est lorem. Quam elementum pulvinar etiam non quam lacus suspendisse faucibus interdum. Ac tortor dignissim convallis aenean et tortor at. Tristique risus nec feugiat in fermentum posuere. Habitasse platea dictumst quisque sagittis purus sit. Tempor commodo ullamcorper a lacus vestibulum. Condimentum id venenatis a condimentum vitae sapien pellentesque habitant. Sem et tortor consequat id porta nibh venenatis. Sem viverra aliquet eget sit amet tellus cras adipiscing enim. Quam nulla porttitor massa id neque aliquam. Dignissim convallis aenean et tortor at risus viverra adipiscing. Et malesuada fames ac turpis egestas integer eget. Consectetur adipiscing elit ut aliquam purus sit amet. Eget nulla facilisi etiam dignissim diam quis enim lobortis. Scelerisque purus semper eget duis at tellus at urna. Lacus vestibulum sed arcu non odio euismod lacinia at quis. Aliquet enim tortor at auctor urna.</p>
           <p>Urna porttitor rhoncus dolor purus non enim praesent. Enim blandit volutpat maecenas volutpat blandit aliquam etiam. Scelerisque eu ultrices vitae auctor eu augue ut lectus arcu. Aliquet risus feugiat in ante metus dictum at tempor. Praesent tristique magna sit amet purus gravida quis blandit turpis. Elementum eu facilisis sed odio morbi quis commodo. Pulvinar elementum integer enim neque volutpat ac. Sapien pellentesque habitant morbi tristique. Risus ultricies tristique nulla aliquet enim tortor at. Integer quis auctor elit sed vulputate mi. Tempus egestas sed sed risus. Tellus integer feugiat scelerisque varius morbi enim. Ac ut consequat semper viverra nam libero. Massa massa ultricies mi quis hendrerit dolor. Viverra vitae congue eu consequat ac felis donec et odio. Et leo duis ut diam quam nulla. Mi quis hendrerit dolor magna eget est lorem. Mauris vitae ultricies leo integer. Ac felis donec et odio pellentesque diam volutpat. Etiam erat velit scelerisque in dictum non consectetur a erat.</p>
         </div>
@@ -58,13 +62,35 @@
         </ul>
       </div>
       <div class="course-list">
-        <p class="font-bold text-3xl mb-4">Courses</p>
+        <div class="course-header mb-4">
+          <p class="font-bold text-3xl">Courses</p>
+          <select name="sort" id="sort" value="dateAsc">
+            <option value="dateAsc">Date (New)</option>
+            <option value="dateDesc">Date (Old)</option>
+            <option value="priceAsc">Price (Low)</option>
+            <option value="priceDesc">Price (High)</option>
+          </select>
+        </div>
         <div class="course-grid">
-          <article v-for="i in 10" class="course-card">
-            <div class="image-container"></div>
-            <p class="title text-xl">Course {{ i }}</p>
-            <p>29.99$</p>
-          </article>
+          <router-link to="/" v-for="course in courseList" :key="course.id">
+            <article class="course-card">
+              <div class="image-container">
+                <img :src="course.img" alt="">
+              </div>
+              <p class="title text-lg">{{ course.name }}</p>
+              <div class="rating">
+                <p>{{ course.rating }}</p>
+                <div class="stars">
+                  <Icon name="mingcute:star-fill" />
+                  <Icon name="mingcute:star-fill" />
+                  <Icon name="mingcute:star-fill" />
+                  <Icon name="mingcute:star-fill" />
+                  <Icon name="mingcute:star-fill" />
+                </div>
+              </div>
+              <p class="font-light text-sm">{{ course.price }}$</p>
+            </article>
+          </router-link>
         </div>
       </div>
     </div>
@@ -73,7 +99,36 @@
 
 <script lang="ts">
 export default {
-  name: "[instructor-id]"
+  name: "[instructor-id]",
+  setup() {
+    const courseList = [
+      {
+        id: 1,
+        img: "https://img-b.udemycdn.com/course/240x135/2776760_f176_10.jpg",
+        name: "The Complete 2023 Web Development Bootcamp",
+        price: 20.99,
+        rating: 4.6,
+      },
+      {
+        id: 2,
+        img: "https://img-b.udemycdn.com/course/240x135/3142166_a637_3.jpg",
+        name: "100 Days of Code: The Complete Python Pro Bootcamp for 2023",
+        price: 12.99,
+        rating: 4.6,
+      },
+      {
+        id: 3,
+        img: "https://img-b.udemycdn.com/course/240x135/1565838_e54e_16.jpg",
+        name: "[NEW] Ultimate AWS Certified Cloud Practitioner - 2023",
+        price: 8.99,
+        rating: 4.6,
+      }
+    ]
+
+    return {
+      courseList
+    };
+  }
 }
 </script>
 
@@ -91,6 +146,14 @@ export default {
           background-color: red;
           border-radius: 50%;
           overflow: hidden;
+          box-shadow: 0 0 5px 0 rgba(255,255,255,0.75);
+
+          img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            object-position: center;
+          }
         }
 
         .links {
@@ -110,6 +173,7 @@ export default {
               align-items: center;
               justify-content: center;
               transition: background-color 0.3s ease;
+              gap: 8px;
 
               &:hover {
                 background-color: rgba(255, 255, 255, 0.1);
@@ -153,6 +217,7 @@ export default {
 
       .categories {
         width: 256px;
+        flex-shrink: 0;
 
         ul {
           li {
@@ -172,6 +237,18 @@ export default {
       .course-list {
         flex-grow: 1;
 
+        .course-header {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+
+          select {
+            color: black;
+            outline: none;
+            padding: 4px;
+          }
+        }
+
         .course-grid {
           width: 100%;
           display: grid;
@@ -183,8 +260,25 @@ export default {
 
             .image-container {
               width: 100%;
-              height: 196px;
-              background-color: blue;
+
+              img {
+                width: 100%;
+                object-position: center;
+                object-fit: cover;
+              }
+            }
+
+            .rating {
+              display: flex;
+              align-items: center;
+              gap: 4px;
+
+              .stars {
+                display: flex;
+                align-items: center;
+                gap: 2px;
+                color: gold;
+              }
             }
           }
         }
